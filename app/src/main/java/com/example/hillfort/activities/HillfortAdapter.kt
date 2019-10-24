d07847d47129d06af799a7c49c7208154932fe85
root <root@LAPTOP-RHTE7Q8H.localdomain>
2019-10-24 23:33:10 +0100

    hillfort list

---
+++ app/src/main/java/com/example/hillfort/activities/HillfortAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.hillfort.activities
+
+class HillfortAdapter {
+}
---
+++ app/src/main/java/com/example/hillfort/activities/HillfortList.kt
@@ -0,0 +1,12 @@
+package com.example.hillfort.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HillfortList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hillfort_list)
+    }
+}
